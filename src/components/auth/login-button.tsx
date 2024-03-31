1ebdcba54c646a9c3ae90a629312ad8e40cd7270
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-03-31 20:30:36 +0200

    feat: add login form component

---
+++ src/components/auth/login-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+type LoginButtonProps = {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+};
+
+export default function LoginButton({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/login");
+  };
+
+  if (mode === "modal") {
+    return <span>todo: implement modal</span>;
+  }
+  return (
+    <span onClick={handleClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+}
---
+++ src/components/auth/login-form.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { CardWrapper } from "./card-wrapper";
+import { FormError } from "@/components/form-error";
+import { FormSucess } from "../form-sucess";
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="john@gmail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" placeholder="********" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message="Invalid credentials" />
+          <Button
+            variant="secondary"
+            type="submit"
+            className="w-full rounded-lg"
+          >
+            Login
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
