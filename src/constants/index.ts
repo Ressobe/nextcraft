47ca26a30efe77b68197b6794529d8abef85903e
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-05-25 22:05:00 +0200

    feat: add app info in constants

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = "Nextcraft";
+export const APP_SHORT_DESCRIPTION = "Next.js starter kit";
+export const APP_LONG_DESCRIPTION =
+  "Empower your web development journey with our Next.js starter kit—unleash speed, scalability, and SEO prowess effortlessly!";
