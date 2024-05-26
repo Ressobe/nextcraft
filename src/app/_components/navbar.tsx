e864e3f4727de1ab977d209bc8abd790b9ccf102
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-05-26 21:21:26 +0200

    feat: add sign in button in nabar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,14 +1,21 @@
+import LoginButton from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/constants";
 import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="bg-background brightness-125 flex justify-between items-center p-3 w-full border-b border-purple-500/50 transition-all hover:border-purple-500/90">
+    <nav className="bg-background flex justify-between items-center p-4 w-full border-b border-purple-500/50">
       <Link href="/" className="flex items-center gap-x-2 font-bold">
         <h1 className="font-bold hidden md:block text-2xl text-gradient bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:brightness-75 ">
           {APP_NAME}
         </h1>
       </Link>
+      <LoginButton>
+        <Button className="transform transition-all active:scale-110 hover:bg-muted-foreground ">
+          Sign in
+        </Button>
+      </LoginButton>
     </nav>
   );
 }
